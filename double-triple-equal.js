144d996fc31e6169da0d7790ca57cdc43ae3312d
MohonSaha <practicewithmohon@gmail.com>
2023-02-28 11:57:06 +0600

    Doule equal and triple equal

---
+++ double-triple-equal.js
@@ -0,0 +1,14 @@
+
+/*
+#Things have to keep in mind:
+1) Always use ===
+2) Equal comparison doesn't work fornon-primitive
+
+
+*/
+
+// type conversion
+// type coercion
+// type casting
+
+// Special Note: primitive: Compare value, Non primitive: Compare reference.
